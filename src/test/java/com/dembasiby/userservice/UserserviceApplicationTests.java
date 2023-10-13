68ee3617d41cfad22aa8bafbb9baaf7bb4d47227
Demba Siby <dembasiby@gmail.com>
2023-10-13 18:14:07 +0000

    Initial commit

---
+++ src/test/java/com/dembasiby/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dembasiby.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
