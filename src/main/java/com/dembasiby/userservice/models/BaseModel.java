8e41227e0e5448050da1ee1293011156d29a961e
Demba Siby <dembasiby@gmail.com>
2023-10-16 22:11:55 +0000

    Updated the type of id to Long

--- src/main/java/com/dembasiby/userservice/models/BaseModel.java
+++ src/main/java/com/dembasiby/userservice/models/BaseModel.java
@@ -1,18 +1,17 @@
 package com.dembasiby.userservice.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GenericGenerator(name = "UUID", type = org.hibernate.id.uuid.UuidGenerator.class)
-    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
-    private UUID uuid;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
